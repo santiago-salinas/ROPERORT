e0d04225185f0e157ee34a6f10a2cd09d36a6ccd
Santiago <santiagosalinasoto@gmail.com>
2023-09-06 22:42:30 -0300

    [Green] CartTests

--- Codigo/POS/Rest Api/Models/Cart.cs
+++ Codigo/POS/Rest Api/Models/Cart.cs
@@ -2,7 +2,33 @@ namespace Rest_Api.Models
 {
     public class Cart
     {
-        public User Client { get; set; }
-        public List<BoughtProduct> Products { get; set; }
+        public List<CartLine>? Products { get; set; }
+
+
+        public double PriceUYU
+        {
+            get { return TotalPrice(); }
+        }
+
+        private double TotalPrice()
+        {
+            double sum = 0;
+
+            if(Products != null)
+            {
+                foreach (CartLine line in Products)
+                {
+                    sum += line.Product.PriceUYU * line.Quantity;
+                }
+            }
+            
+
+            return sum;
+        }
+    }
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
     }
 }
