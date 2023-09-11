e19e7e1fc9acaa779d8b61888b7864022ac3bb70
Fede PC <federicorodriguez2002@gmail.com>
2023-09-10 21:23:53 -0300

    Refactor EFProductService

--- Codigo/POS/DataAccess/DatabaseServices/EFProductService.cs
+++ Codigo/POS/DataAccess/DatabaseServices/EFProductService.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccess.Entities;
 using DataAccess.Expcetions;
 using Microsoft.EntityFrameworkCore;
+using Rest_Api.Models;
+using Rest_Api.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,21 +12,23 @@ using System.Threading.Tasks;
 
 namespace DataAccess.DatabaseServices
 {
-    public class EFProductService
+    public class EFProductService : ICRUDService<Product>
     {
-        public EFProductService() { }
+        public EFProductService()  { }
 
-        public List<ProductEntity> GetAll()
+        public List<Product> GetAll()
         {
             using (EFContext context = new EFContext())
             {
                 try
                 {
-                    List<ProductEntity> products = context.ProductEntities
+                    List<ProductEntity> entities = context.ProductEntities
                         .Include(p => p.Brand)
                         .Include(p => p.Category)
                         .Include(p => p.Colours.Select(c => c.Colour))
                         .ToList();
+
+                    List<Product> products = entities.Select(p => ProductEntity.FromEntity(p)).ToList();
 
                     return products;
 
@@ -35,7 +39,7 @@ namespace DataAccess.DatabaseServices
             }
         }
 
-        public ProductEntity? Get(string id) 
+        public Product? Get(int id) 
         {
             using (EFContext context = new EFContext())
             {
@@ -45,9 +49,9 @@ namespace DataAccess.DatabaseServices
                         .Include(p => p.Brand)
                         .Include(p => p.Category)
                         .Include(p => p.Colours.Select(c => c.Colour))
-                        .First(p => p.Name == id);
+                        .First(p => p.Id == id);
 
-                    return product;
+                    return ProductEntity.FromEntity(product);
 
                 }
                 catch
@@ -56,15 +60,16 @@ namespace DataAccess.DatabaseServices
                 }
 
             }
-
         }
 
-        public void Add(ProductEntity entity)
+        public void Add(Product product)
         {
             try
             {
                 using (EFContext context = new EFContext())
                 {
+                    ProductEntity entity = ProductEntity.FromModel(product);
+
                     context.ProductEntities.Add(entity);
                     context.SaveChanges();
                 }
@@ -72,17 +77,18 @@ namespace DataAccess.DatabaseServices
             catch
             {
             
-                throw new DatabaseException("Error while trying to add product " + entity.Name);
+                throw new DatabaseException("Error while trying to add product " + product.Name);
             }
         }
 
-        public void Delete(string id)
+        public void Delete(int id)
         {
             try
             {
                 using (EFContext context = new EFContext())
                 {
-                    context.ProductEntities.Remove(Get(id));
+                    ProductEntity entity = context.ProductEntities.First(p => p.Id == id);
+                    context.ProductEntities.Remove(entity);
                 }
 
             }
@@ -92,10 +98,11 @@ namespace DataAccess.DatabaseServices
             }
         }
 
-        public void Update(ProductEntity entity)
+        public void Update(Product product)
         {
             try
             {
+                ProductEntity entity = ProductEntity.FromModel(product);
                 using (EFContext context = new EFContext())
                 {
                     context.ProductEntities.Update(entity);
@@ -103,7 +110,7 @@ namespace DataAccess.DatabaseServices
             }
             catch 
             {
-                throw new DatabaseException("Error while trying to update product " + entity.Name);
+                throw new DatabaseException("Error while trying to update product " + product.Name);
             }
         }
     }
