558e3ff7a861f07824a3b6f595c5385c110946b3
Santiago <santiagosalinasoto@gmail.com>
2023-09-13 19:24:22 -0300

    [Red] TotalLookPromo ApplyDiscount

--- Codigo/POS/Rest Api/Models/Promos/TotalLookPromo.cs
+++ Codigo/POS/Rest Api/Models/Promos/TotalLookPromo.cs
@@ -28,50 +28,37 @@ namespace Rest_Api.Models.Promos
                 }
             }
 
+
+            foreach (Colour colour in colorsInCart)
+            {
+                List<CartLine> cartLinesWithColor = cart.Products
+                .Where(cartLine =>
+                    cartLine.Product != null &&
+                    cartLine.Product.Colours != null &&
+                    cartLine.Product.Colours.Contains(colour))
+                .ToList();
+
+                if(cartLinesWithColor.Count >= 3)
+                {
+                    cartLinesWithColor.OrderBy(p => p.Product.PriceUYU);
+                }
+
+                int amountOfParticipatingProducts = 0;
+                List<Product> participatingProducts = new List<Product>();
+
+                while(amountOfParticipatingProducts<3 && cartLinesWithColor.Count > 0)
+                {
+                    participatingProducts.Add(cartLinesWithColor.First().Product);
+                    amountOfParticipatingProducts += cartLinesWithColor.First().Quantity;
+                    cartLinesWithColor.Remove(cartLinesWithColor.First());
+                }
+
+                return retValue - (participatingProducts.Last().PriceUYU/2.0);
+            }
+
             return retValue;
         }
 
-        private bool HasThreeSameColoredProducts(List<CartLine> products)
-        {
-            int amountOfItems = 0;
-            foreach (CartLine line in products)
-            {
-                amountOfItems += line.Quantity;
-                if (amountOfItems == 2) return true;
-            }
-
-            return false;
-        }
-
-        private List<Colour> ColoursInCommon(Product productA, Product productB)
-        {
-            return productA.Colours.Intersect(productB.Colours).ToList();
-        }
-
-        private bool HasThreeColoursInCommon(List<CartLine> lines)
-        {
-
-            for (int i = 0; i < lines.Count; i++)
-            {
-                for (int j = i + 1; j < lines.Count; j++)
-                {
-                    var commonColors = lines[i].Product.Colours.Intersect(lines[j].Product.Colours);
-
-                    if (commonColors.Any())
-                    {
-                        for (int k = j + 1; k < lines.Count; k++)
-                        {
-                            if (commonColors.Intersect(lines[k].Product.Colours).Any())
-                            {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
     }
 
 
