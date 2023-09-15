0235bd0f0d6847a1d7def0d207b6a67c8c634e13
Santiago <santiagosalinasoto@gmail.com>
2023-09-15 14:49:41 -0300

    [Red] DiscountApplierTest

--- Codigo/POS/ApiTests/Controllers/CartControllerTest.cs
+++ Codigo/POS/ApiTests/Controllers/CartControllerTest.cs
@@ -218,5 +218,59 @@ namespace ApiTests
             Assert.AreEqual(400, createdResult.StatusCode);
 
         }
+
+        [TestMethod]
+        public void CartDiscountAppliedSuccess()
+        {
+            Brand brand = new Brand();
+            brand.Name = "Adidas";
+
+            Category category = new Category();
+            category.Name = "Shorts";
+
+            Colour colour = new Colour();
+            colour.Name = "Red";
+
+            List<Colour> colours = new List<Colour>
+            {
+                colour
+            };
+            
+            // 3 productos -> 20% highest (1680)
+            // 3 mismo color -> 1500
+            // 3 misma categoria -> 1200
+            // 3 misma brand -> 600
+            var mock = new Mock<ICRUDService<Product>>(MockBehavior.Strict);
+            mock.Setup(s => s.Get(1)).Returns(new Product
+            {
+                Id = 1,
+                Name = "Cap1",
+                PriceUYU = 600,
+                Description = "Stylish Cap.",
+                Brand = brand,
+                Category = category,
+                Colours = colours
+            });
+            var controller = new CartController(mock.Object);
+
+            CartDTO cartDto = new CartDTO();
+            CartLineDTO cartLineDto = new CartLineDTO()
+            {
+                id = 1,
+                Quantity = 3
+            };
+
+            cartDto.Products.Add(cartLineDto);
+
+            var result = controller.Create(cartDto);
+
+            var createdResult = result as CreatedAtActionResult;
+
+            Cart createdCart = (Cart)createdResult.Value;
+            double originalValue = 600;
+            double expectedValue = 600;
+            Assert.AreEqual(expectedValue, createdCart.PriceUYU);
+
+        }
     }
 }
