0ee5060b81408b0c7a1904f0e83049ad9c287503
Fede Laptop <federicorodriguez2002@gmail.com>
2023-09-27 09:41:26 -0300

    [Red] Purchase controller and service tests

---
+++ Codigo/POS/ApiTests/Controllers/PurchaseControllerTests.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Rest_Api.Controllers;
+using Services;
+using Services.Exceptions;
+using Services.Interfaces;
+using Services.Models;
+
+namespace ApiTests.Controllers
+{
+    [TestClass]
+    public class PurchaseControllerTest
+    {
+        private Mock<IPurchaseService> mock;
+        private PurchaseController _purchaseController;
+        private User _testUser;
+        private User _testUserTwo;
+
+        private Cart _testCart;
+        private Cart _testCartTwo;
+
+        private Purchase _testPurchase;
+        private Purchase _testPurchaseTwo;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mock = new Mock<IPurchaseService>(MockBehavior.Strict);
+            _purchaseController = new PurchaseController(mock.Object);
+
+            _testCart = new Cart();
+            Product product1 = new Product();
+            product1.PriceUYU = 10.0; 
+
+            Product product2 = new Product();
+            product2.PriceUYU = 15.0; 
+
+            var promo = new TwentyPercentOff();
+            CartLine cartLine1 = new CartLine();
+            cartLine1.Product = product1;
+            cartLine1.Quantity = 2; 
+
+            CartLine cartLine2 = new CartLine();
+            cartLine2.Product = product2;
+            cartLine2.Quantity = 3;
+
+            _testCart = new Cart();
+
+            _testCart.Products.Add(cartLine1);
+            _testCart.Products.Add(cartLine2);
+
+            _testCart.AppliedPromo = promo;
+
+            _testUser = new User(1, "email1@gmail.com", "address1"); 
+            _testUserTwo = new User(2, "email2@gmail.com", "address2");
+
+            _testCart = new Cart();
+            _testCartTwo = new Cart();
+
+            _testPurchase = new Purchase
+            {
+                Id = 1,
+                Client = _testUser,
+                Products = _testCart,
+                Date = DateTime.Now,
+                AppliedPromotion = "Promo1"
+            };
+
+            _testPurchaseTwo = new Purchase
+            {
+                Id = 2,
+                Client = _testUserTwo,
+                Products = _testCartTwo,
+                Date = DateTime.Now,
+                AppliedPromotion = "Promo2"
+            };
+        }
+
+        [TestMethod]
+        public void GetAll_WorksCorrectly()
+        {
+            var expectedOutcome = new List<Purchase>() { _testPurchase, _testPurchaseTwo};
+            mock.Setup(s => s.GetAll()).Returns(expectedOutcome);
+            var result = _purchaseController.GetAll();
+            var createdResult = result as ActionResult<List<Purchase>>;
+            Assert.AreEqual(expectedOutcome.Count, createdResult.Value.Count);
+        }
+
+        [TestMethod]
+        public void GivenValidId_GetReturnsUser()
+        {
+            var expectedOutcome = _testPurchase;
+            mock.Setup(s => s.Get(5)).Returns(expectedOutcome);
+            var result = _purchaseController.Get(5);
+            var createdResult = result as ActionResult<Purchase>;
+            Assert.AreEqual(expectedOutcome, createdResult.Value);
+        }
+
+        [TestMethod]
+        public void GivenInvalidId_GetReturnsNotFound()
+        {
+            Purchase? nullPurchase = null;
+            mock.Setup(s => s.Get(7)).Returns(nullPurchase);
+            var result = _purchaseController.Get(7);
+            Assert.AreEqual(result.Value, null);
+        }
+
+        [TestMethod]
+        public void GivenValidPurchase_ItGetsCreated()
+        {
+            Purchase purchase = _testPurchase;
+            mock.Setup(s => s.Add(purchase));
+            var result = _purchaseController.Create(purchase);
+            var createdResult = result as OkResult;
+            Assert.AreEqual(createdResult.StatusCode, 200);
+        }
+
+        [TestMethod]
+        public void GivenInvalidPurchase_Create_ReturnsBadRequest()
+        {
+            var purchase = _testPurchase;
+            mock.Setup(s => s.Add(purchase)).Throws(new Service_ObjectHandlingException(""));
+            var result = _purchaseController.Create(purchase);
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
+
+        [TestMethod]
+        public void GivenValidEmail_GetPurchaseHistory_ReturnsPurchasesFromGivenUser()
+        {
+            Purchase purchaseUserOne = new Purchase
+            {
+                Id = 2,
+                Client = _testUser,
+                Products = _testCartTwo,
+                Date = DateTime.Now,
+                AppliedPromotion = "Promo2"
+            };
+            Purchase purchaseUserTwo = new Purchase
+            {
+                Id = 2,
+                Client = _testUserTwo,
+                Products = _testCartTwo,
+                Date = DateTime.Now,
+                AppliedPromotion = "Promo2"
+            };
+
+            List<Purchase> getAll = new List<Purchase>() { purchaseUserOne, purchaseUserTwo };
+            List<Purchase> purchaseHistory = new List<Purchase>() { purchaseUserOne };
+            mock.Setup(s => s.GetPurchaseHistory(_testUser.Address)).Returns(purchaseHistory);
+            var result = _purchaseController.GetPurchaseHistory(_testUser.Address);
+            var createdResult = result as ActionResult<List<Purchase>>;
+            Assert.AreEqual(purchaseHistory, createdResult.Value);
+        }
+
+    }
+}
---
+++ Codigo/POS/ApiTests/Services/PurchaseServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using Moq;
+using Services;
+using Services.Models;
+using Services.Interfaces;
+
+namespace ApiTests.Services
+{
+    [TestClass]
+    public class PurchaseServiceTests
+    {
+        private PurchaseService _purchaseService;
+        private Mock<ICRUDRepository<Purchase>> _purchaseRepository;
+
+        private User _testUser;
+        private User _testUserTwo;
+
+        private Cart _testCart;
+        private Cart _testCartTwo;
+
+        private Purchase _testPurchase;
+        private Purchase _testPurchaseTwo;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _purchaseRepository = new Mock<ICRUDRepository<Purchase>>();
+            _purchaseService = new PurchaseService(_purchaseRepository.Object);
+
+            _testCart = new Cart();
+            Product product1 = new Product();
+            product1.PriceUYU = 10.0;
+
+            Product product2 = new Product();
+            product2.PriceUYU = 15.0;
+
+            var promo = new TwentyPercentOff();
+            CartLine cartLine1 = new CartLine();
+            cartLine1.Product = product1;
+            cartLine1.Quantity = 2;
+
+            CartLine cartLine2 = new CartLine();
+            cartLine2.Product = product2;
+            cartLine2.Quantity = 3;
+
+            _testCart = new Cart();
+
+            _testCart.Products.Add(cartLine1);
+            _testCart.Products.Add(cartLine2);
+
+            _testCart.AppliedPromo = promo;
+
+            _testUser = new User(1, "email1@gmail.com", "address1");
+            _testUserTwo = new User(2, "email2@gmail.com", "address2");
+
+            _testCart = new Cart();
+            _testCartTwo = new Cart();
+
+            _testPurchase = new Purchase
+            {
+                Id = 1,
+                Client = _testUser,
+                Products = _testCart,
+                Date = DateTime.Now,
+                AppliedPromotion = "Promo1"
+            };
+
+            _testPurchaseTwo = new Purchase
+            {
+                Id = 2,
+                Client = _testUserTwo,
+                Products = _testCartTwo,
+                Date = DateTime.Now,
+                AppliedPromotion = "Promo2"
+            };
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsListOfPurchases()
+        {
+            var purchaseList = new List<Purchase>
+            {
+                _testPurchase,
+                _testPurchaseTwo                
+            };
+            _purchaseRepository.Setup(repo => repo.GetAll()).Returns(purchaseList);
+
+            var purchases = _purchaseService.GetAll();
+
+            Assert.IsNotNull(purchases);
+            Assert.AreEqual(purchaseList.Count, purchases.Count);
+        }
+
+        [TestMethod]
+        public void Get_ReturnsPurchaseById()
+        {
+            var id = 1;
+            var expectedPurchase = _testPurchase;
+            _purchaseRepository.Setup(repo => repo.Get(id)).Returns(expectedPurchase);
+
+            var purchase = _purchaseService.Get(id);
+
+            Assert.IsNotNull(purchase);
+            Assert.AreEqual(expectedPurchase.Id, purchase.Id);
+        }
+
+        [TestMethod]
+        public void Add_PuchaseIsAddedToList()
+        {
+            var newPurchase = _testPurchase;
+            _purchaseRepository.Setup(repo => repo.Add(newPurchase));
+            _purchaseRepository.Setup(repo => repo.Get(newPurchase.Id)).Returns(newPurchase);
+
+            _purchaseService.Add(newPurchase);
+            var addedPurchase = _purchaseService.Get(newPurchase.Id);
+
+            Assert.IsNotNull(addedPurchase);
+            Assert.AreEqual(newPurchase.Id, addedPurchase.Id);
+        }        
+    }
+}
