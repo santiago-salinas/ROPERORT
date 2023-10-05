6379572fd50392a106f49143b611dcea3ea50a51
Santiago <santiagosalinasoto@gmail.com>
2023-10-05 14:20:40 -0300

    [Deleted] Ghost Files & Folders

--- Codigo/POS/Rest Api/Models/Brand.cs
+++
@@ -1,9 +0,0 @@
-namespace Rest_Api.Models
-{
-    public class Brand
-    {
-        public Brand() { }
-        public Brand(string name) { Name = name; }
-        public string Name { get; set; }
-    }
-}
--- Codigo/POS/Rest Api/Models/Exceptions/ModelsExceptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Rest_Api.Models.Exceptions
-{
-    public class Models_ArgumentException : Exception
-    {
-        public Models_ArgumentException(string message) : base(message) { }
-    }
-}
