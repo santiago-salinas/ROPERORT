e235a032954db01e4b69e7b67898cf9acaa3d6ef
Manuel Morandi <manumorandi1411@gmail.com>
2023-10-11 17:53:28 -0300

    [Green] Credit Card

---
+++ Codigo/POS/ApiTests/Models/PaymentMethodsTests.cs
@@ -0,0 +1,51 @@
+﻿using Services.Exceptions;
+using Services.Models.PaymentMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTests.Models
+{
+    [TestClass]
+    public class PaymentMethodsTests
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+
+        }
+
+        [TestMethod]
+        public void GivenValidCompaniesCreditCardWorksProperly()
+        {
+            CreditCard c1 = new CreditCard() 
+                { Id = "123456789", Company = "Visa" };
+            CreditCard c2 = new CreditCard() 
+                { Id = "123456789", Company = "MasterCard" };
+            Assert.AreEqual(c1.Company, "VISA");
+            Assert.AreEqual(c2.Company, "MASTERCARD");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Service_ArgumentException), "Invalid company")]
+        public void GivenInvalidCompanyCreditCardThrowsException()
+        {
+            CreditCard card = new CreditCard()
+                { Id = "123456789", Company = "NonExistent" };
+        }
+
+        [TestMethod]
+        public void GivenValidBanksDebitWorksProperly()
+        {
+
+        }
+
+        [TestMethod]
+        public void GivenInvalidBankDebitThrowsException()
+        {
+
+        }
+    }
+}
---
+++ Codigo/POS/Services/Models/PaymentMethods/CreditCard.cs
@@ -0,0 +1,33 @@
+﻿using Services.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Models.PaymentMethods
+{
+    public class CreditCard : PaymentMethod
+    {
+        private string _company;
+
+        public string Company
+        { 
+            get => _company;
+            set
+            {
+                ValidateCompany(value);
+                _company = value.ToUpper();
+            }
+        }
+
+        private void ValidateCompany(string company)
+        {
+            string companyAllCaps = company.ToUpper();
+            bool validCompany = companyAllCaps.Equals("VISA")
+                || companyAllCaps.Equals("MASTERCARD");
+            if (!validCompany) 
+                throw new Service_ArgumentException("Invalid company");
+        }
+    }
+}
