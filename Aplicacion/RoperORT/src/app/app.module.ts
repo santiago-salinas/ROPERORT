effed048d0cf57ee79d51df8a5eb688391dcd0aa
Santiago <santiagosalinasoto@gmail.com>
2023-10-13 15:52:07 -0300

    Page with Logo

--- Aplicacion/RoperORT/src/app/app.module.ts
+++ Aplicacion/RoperORT/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
