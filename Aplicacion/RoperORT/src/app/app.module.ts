9e35685e8901ba70c464e4be1626eade490b3433
Santiago <santiagosalinasoto@gmail.com>
2023-10-20 11:11:22 -0300

    Product Card Initial

--- Aplicacion/RoperORT/src/app/app.module.ts
+++ Aplicacion/RoperORT/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
+    ProductCardComponent,
   ],
   imports: [
     BrowserModule,
