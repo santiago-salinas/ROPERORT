801e8b255204bd539ca58e63d25b0ebaca49c152
Santiago <santiagosalinasoto@gmail.com>
2023-10-24 00:00:47 -0300

    Swapped Old with New Routed Pages

--- Aplicacion/RoperORT/src/app/app-routing.module.ts
+++ Aplicacion/RoperORT/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductsComponent } from './pages/products/products.component';
+
 
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/home' }, // Handle 404 errors
 ];
 
 @NgModule({
