f6484a3668d5b67a82b2670efb74c114ab7e7513
Federico Rodriguez - PC <federicorodriguez2002@gmail.com>
2023-10-24 23:49:11 -0300

    Edit product form first draft

--- Aplicacion/RoperORT/src/app/product_management/product-admin-card/product-admin-card.component.ts
+++ Aplicacion/RoperORT/src/app/product_management/product-admin-card/product-admin-card.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {Input} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { Product } from 'src/app/models/product.model';
-import {MatDividerModule} from '@angular/material/divider';
-
+import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
+import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import { ProductFormComponent } from '../product-form/product-form.component';
 
 
 
@@ -13,16 +15,29 @@ import {MatDividerModule} from '@angular/material/divider';
   selector: 'app-product-admin-card',
   templateUrl: './product-admin-card.component.html',
   styleUrls: ['./product-admin-card.component.scss'],
-  imports: [MatCardModule, MatButtonModule,MatIconModule,MatDividerModule],
+  imports: [MatCardModule, MatButtonModule,MatIconModule,MatDividerModule, CommonModule, MatDialogModule],
   standalone: true,
 })
 export class ProductAdminCardComponent {
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   @Input() productDetails: Product = new Product();
 
   getColoursNames(): string {
     return this.productDetails.colours.map((colour) => colour.name).join(', ');
+  }
+
+  editDialog(): void {
+    let dialogRef = this.dialog.open(ProductFormComponent, {
+      data: { product: this.productDetails },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log(`Dialog result: ${result}`);
+      }
+    });
+
   }
 }
 
--- Aplicacion/RoperORT/src/app/product_management/product-form/product-form.component.ts
+++ Aplicacion/RoperORT/src/app/product_management/product-form/product-form.component.ts
@@ -1,37 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { Product } from '../models/product.model';
+import { Product } from 'src/app/models/product.model';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import {MAT_DIALOG_DATA, MatDialogRef, } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss'],
   standalone: true,
-  imports: [MatInputModule, MatButtonModule, FormsModule, MatSelectModule, MatFormFieldModule, ReactiveFormsModule],
+  imports: [MatInputModule, MatButtonModule, FormsModule, MatSelectModule, MatFormFieldModule, ReactiveFormsModule, CommonModule],
 
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent implements OnInit{
   productForm: FormGroup = new FormGroup({});
+  availableColors: string[] = ['Red', 'Blue', 'Green', 'Yellow', 'Black'];
+  productDetails: any = new Product();
+  name: string = 'asd';
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  @Input() productDetails:Product = new Product();
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<ProductFormComponent>, @Inject(MAT_DIALOG_DATA) public data: Product) {
+    this.productDetails = this.data;
     this.productForm = this.formBuilder.group({
-      name: [this.productDetails.name],
-      description: [this.productDetails.description],
-      price: [this.productDetails.priceUYU],
-      stock: [this.productDetails.stock],
-      brand: [this.productDetails.brand],
-      category: [this.productDetails.category],
-      colors: [this.productDetails.colours],
+      name: [this.productDetails.product.name],
+      colors: [this.productDetails.product.colours],
     });
+  }
+
+  ngOnInit(): void {
+    this.name = this.productDetails.product.name;
+
   }
 
   submitForm() {}
