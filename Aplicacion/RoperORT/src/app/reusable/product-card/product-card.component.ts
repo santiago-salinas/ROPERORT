cfe7c942a22f3febd4de017963ddf2c6652a34d4
Santiago <santiagosalinasoto@gmail.com>
2023-10-25 08:53:46 -0300

    Product card changes depending on route

--- Aplicacion/RoperORT/src/app/reusable/product-card/product-card.component.ts
+++ Aplicacion/RoperORT/src/app/reusable/product-card/product-card.component.ts
@@ -1,21 +1,24 @@
 import {Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { Product } from 'src/app/models/product.model';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,MatIconModule,MatSnackBarModule],
+  imports: [CommonModule,MatCardModule, MatButtonModule,MatIconModule,MatSnackBarModule],
 })
 
 
 export class ProductCardComponent {
-  constructor(private _snackBar: MatSnackBar) {}
+  constructor(private _snackBar: MatSnackBar, private router: Router) {}
 
   @Input() productDetails: Product = new Product();
 
@@ -29,7 +32,25 @@ export class ProductCardComponent {
     });
   }
 
+  removeFromCart() {
+    const message = `Not Implemented`;
+    const action = 'Close';
+
+    // Open the snack bar
+    this._snackBar.open(message, action, {
+      duration: 3000, // Specify the duration in milliseconds
+    });
+  }
+
   getColoursNames(): string {
     return this.productDetails.colours.map((colour) => colour.name).join(', ');
+  }
+
+  isProductsRoute(): boolean {
+    return this.router.url === '/products';
+  }
+
+  isCartRoute(): boolean {
+    return this.router.url === '/cart';
   }
 }
