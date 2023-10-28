770d4086085d77953847c460782c60f83d6bc37e
Manuel Morandi <manumorandi1411@gmail.com>
2023-10-28 13:32:50 -0300

    Improved error shown when non Admin enters Admin Module

--- Aplicacion/RoperORT/src/app/pages/user-admin/user-admin.component.ts
+++ Aplicacion/RoperORT/src/app/pages/user-admin/user-admin.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { LoginService } from 'src/app/services/data.service';
@@ -12,13 +13,13 @@ import { UserCardComponent } from 'src/app/reusable/user-card/user-card.componen
   templateUrl: './user-admin.component.html',
   styleUrls: ['./user-admin.component.scss'],
   standalone: true,
-  imports: [CommonModule,MatIconModule,MatButtonModule,UserCardComponent]
+  imports: [CommonModule,MatIconModule,MatButtonModule,UserCardComponent,MatSnackBarModule]
 })
 
 export class UserAdminComponent {
   userList : User[];
 
-  constructor(private dataService: LoginService, private router: Router) {
+  constructor(private dataService: LoginService, private router: Router, private _snackBar: MatSnackBar) {
     this.userList = [];
   }
 
@@ -28,13 +29,24 @@ export class UserAdminComponent {
         this.userList = data;
       },
       (error:any) => {
-        alert("Access is exclusive to Admins");
-        this.router.navigate(['/home']);
+        if(error.status === 403){
+          this.showSnackbar("Access is exclusive to Admins", "Close", 3000);
+          this.router.navigate(['/home']);
+        } else {
+          alert('API Is Not Responding. Reloading after OK');
+          location.reload();
+        }
       }
     );
   }
 
   addUser(){
     this.router.navigate(['/user-creation']);
+  }
+
+  showSnackbar(message: string, action: string, duration: number){
+    this._snackBar.open(message, action, {
+      duration: duration,
+    });
   }
 }
