0f799f4a9dde3af1790739e5db0b11c3742806bc
Manuel Morandi <manumorandi1411@gmail.com>
2023-11-09 19:41:30 -0300

    Non admins now get redirected when trying to enter admin module

--- Aplicacion/RoperORT/src/app/guards/admin.guard.ts
+++ Aplicacion/RoperORT/src/app/guards/admin.guard.ts
@@ -1,9 +1,16 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { LoginService } from '../services/data.service';
+import { Router } from '@angular/router';
 import { inject } from '@angular/core';
-
-export const AdminGuard: CanActivateFn =
-    (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-      return inject(LoginService).isAdmin();
-    };
+    
+export const AdminGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> => {
+  const loginService = inject(LoginService);
+  const router = inject(Router);
+  return loginService.isAdmin().then((isAdmin) => {
+    if (isAdmin) {
+      return true; 
+    } else {
+      return router.createUrlTree(['/home']); 
+    }
+  });
+}; 
