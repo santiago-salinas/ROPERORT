f74626bb59ceee057c4a844372c9741a49389142
Federico Rodriguez - PC <federicorodriguez2002@gmail.com>
2023-11-15 06:03:53 -0300

    Cart frontend fix

--- Codigo/POS/Promotions/Interfaces/IPaymentMethod.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Promotions.Interfaces
-{
-    public interface IPaymentMethod
-    {
-        public string Id { get; set; }
-        public double ApplyDiscount();        
-        public string GetType();
-    }
-}
